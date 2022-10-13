9cf892b9fd7c89dc116fb9e6b93bf87433778164
Eran Sevil <Erans@xforex.com>
2022-10-13 14:59:48 +0300

    F

--- App.js
+++ App.js
@@ -10,14 +10,17 @@ import {
   ViroAnimations,
   Viro3DObject,
   ViroAmbientLight,
+  ViroARTrackingTargets,
+  ViroARImageMarker
 } from '@viro-community/react-viro';
 
-// const HelloWorldSceneAR = () => {
+const ARCmps = () => {
 //   const [text, setText] = useState('Initializing AR...');
 
 const InitialScene = props => {
   const [rotation, setRotation] = useState([-70, 0, 0]);
   const [position, setPosition] = useState([0, -3, -5]);
+  const [scale, setScale] = useState([0.015, 0.015, 0.015]);
   let data = props.sceneNavigator.viroAppProps;
   ViroMaterials.createMaterials({
     wood: {
@@ -32,7 +35,7 @@ const InitialScene = props => {
   };
 
   const rotateObject = (rotateState, rotationFactor, source) => {
-    if (rotateState === 3) {
+    if (rotateState !== 1) {
       let newRotation = [
         rotation[0] - rotationFactor,
         rotation[1] - rotationFactor,
@@ -45,36 +48,65 @@ const InitialScene = props => {
     }
   };
 
+  const scaleObject = (pinchState, scaleFactor, source) =>{
+
+    if(pinchState ===3){
+      let currentScale = scale[0]
+      let newScale = currentScale * scaleFactor
+      setScale([newScale,newScale,newScale])
+    }
+    
+  }
+
+  const anchorFound = () =>{
+    console.log('anchorFound !!!!');
+  }
   return (
     <ViroARScene>
+      {/* <ViroARImageMarker target='firePlaceImage' onAnchorFound={anchorFound}> */}
       <ViroAmbientLight color="#ffffff" />
       {data.object === 'firePlace1' ? (
-        <Viro3DObject
-          source={require('./assets/firePlace1/13110_Fireplace_v2_l3.obj')}
+        <Viro3DObject 
+          source={{ uri: 'https://res.cloudinary.com/sevilcloud/image/upload/v1665585800/fireplaces/old_stool_j9ihzt.glb' /* './assets/firePlace1/13110_Fireplace_v2_l3.obj' */ }}
           position={position}
-          scale={ [0.015, 0.015, 0.015]  }
+          scale={ scale }
           rotation={/* [-70, 0, 0] */ rotation}
           type="OBJ"
           onDrag={moveObject}
           onRotate={rotateObject}
+          onPinch={scaleObject}
         />
       ) : (
         <Viro3DObject
           source={require('./assets/firePlace2/ojaq.obj')}
           position={position}
-          scale={[0.015, 0.015, 0.015]  }
+          scale={scale }
           rotation={/* [0, 90, 0] */ rotation}
           type="OBJ"
           onDrag={moveObject}
           onRotate={rotateObject}
+          onPinch={scaleObject}
+
         />
       )}
+      {/* </ViroARImageMarker> */}
     </ViroARScene>
   );
 };
 
-export default () => {
+
   const [objectForDispaly, setObjectForDispaly] = useState('firePlace1');
+
+  ViroARTrackingTargets.createTargets({
+    'firePlaceImage':{
+      source:require('./assets/fireplace1.jpg'),
+      orientation: "Up",
+      physicalWidth: 0.165, // real world width in meters  
+      type: 'Image'
+
+    }
+  })
+
 
   const ChangeObject = () => {
     console.log('objectForDispaly', objectForDispaly);
@@ -86,6 +118,7 @@ export default () => {
   return (
     <View style={styles.mainView}>
       <ViroARSceneNavigator
+        autofocu={true}
         initialScene={{
           scene: InitialScene,
         }}
@@ -101,7 +134,8 @@ export default () => {
   );
 };
 
-// }
+
+export default ARCmps;
 
 var styles = StyleSheet.create({
   mainView: {
